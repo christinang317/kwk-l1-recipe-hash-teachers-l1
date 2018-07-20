55798cbdd1137b871476c133309e6b1f66428bfe
Christina Ng <christinasy317@gmail.com>
2018-07-20 04:54:52 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -20,7 +20,7 @@ def recipe_ingredients(perfect_10_recipe)
   perfect_10_recipe.each do |ingredients, amount|
     puts "#{ingredients} with #{amount} much"
   end
-  
+
 end
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
